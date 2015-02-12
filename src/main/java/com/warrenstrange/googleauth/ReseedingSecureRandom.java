c7d3c66aa1bfe67fc7120d2a929b313493803510
Senthil Kumar K <skaruppiah@apigee.com>
2015-02-11 19:31:39 -0800

    simplify locking in ReSeedingSecureRandom and fix 'bitwise add of signed byte value' reported by FindBugs

--- src/main/java/com/warrenstrange/googleauth/ReseedingSecureRandom.java
+++ src/main/java/com/warrenstrange/googleauth/ReseedingSecureRandom.java
@@ -35,7 +35,7 @@ import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Date: 08/04/14
@@ -47,9 +47,7 @@ class ReseedingSecureRandom {
     private static final int MAX_OPERATIONS = 1_000_000;
     private final String provider;
     private final String algorithm;
-    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
-    private final Lock readLock = lock.readLock();
-    private final Lock writeLock = lock.writeLock();
+    private final Lock lock = new ReentrantLock();
     private final AtomicInteger count = new AtomicInteger(0);
     private SecureRandom secureRandom;
 
@@ -117,32 +115,19 @@ class ReseedingSecureRandom {
     }
 
     void nextBytes(byte[] bytes) {
-        readLock.lock();
-
         int currentCount = count.incrementAndGet();
-
         if (currentCount > MAX_OPERATIONS) {
-            readLock.unlock();
-            writeLock.lock();
-
+            lock.lock();
             try {
                 currentCount = count.get();
-
                 if (currentCount > MAX_OPERATIONS) {
                     buildSecureRandom();
                     count.set(0);
                 }
-
-                readLock.lock();
             } finally {
-                writeLock.unlock();
+                lock.unlock();
             }
         }
-
-        try {
-            this.secureRandom.nextBytes(bytes);
-        } finally {
-            readLock.unlock();
-        }
+        this.secureRandom.nextBytes(bytes);
     }
 }
