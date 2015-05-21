175a28862bc10b7b776375d15dc79288a6a90f45
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2015-05-21 12:54:34 +0200

    Improve exception messages and update copyright.

--- src/main/java/com/warrenstrange/googleauth/GoogleAuthenticator.java
+++ src/main/java/com/warrenstrange/googleauth/GoogleAuthenticator.java
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 2014-2015 Enrico Maria Crisostomo
+ * Copyright (c) 2014-2015 Enrico M. Crisostomo
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
+ *   * Redistributions of source code must retain the above copyright notice, this
+ *     list of conditions and the following disclaimer.
  *
  *   * Redistributions in binary form must reproduce the above copyright notice,
  *     this list of conditions and the following disclaimer in the documentation
