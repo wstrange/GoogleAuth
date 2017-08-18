e8b2ad791c3b2c470b74fe8d2721dd739811303d
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2017-08-18 13:48:12 +0200

    Bump copyrights

--- src/main/java/com/warrenstrange/googleauth/GoogleAuthenticator.java
+++ src/main/java/com/warrenstrange/googleauth/GoogleAuthenticator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2016 Enrico M. Crisostomo
+ * Copyright (c) 2014-2017 Enrico M. Crisostomo
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
