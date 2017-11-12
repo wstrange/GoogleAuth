350c0d122193a6b19a7b5295a5bffac4e1d50788
Enrico Maria Crisostomo <enrico.m.crisostomo@gmail.com>
2017-11-12 14:52:08 +0100

    Remove deprecated GoogleAuthenticatorKey::getQRBarcodeURL method

--- src/main/java/com/warrenstrange/googleauth/GoogleAuthenticatorKey.java
+++ src/main/java/com/warrenstrange/googleauth/GoogleAuthenticatorKey.java
@@ -47,17 +47,8 @@ import java.util.List;
  * @see GoogleAuthenticator
  * @since 1.0
  */
-public final class GoogleAuthenticatorKey {
-
-    /**
-     * The format string to generate the URL of a Google-provided QR bar code.
-     *
-     * @deprecated Use GoogleAuthenticatorQRGenerator instead.
-     */
-    private static final String QR_FORMAT =
-            "https://chart.googleapis.com/chart?chs=200x200&chld=M%%7C0&cht=qr&"
-                    + "chl=otpauth://totp/%s@%s%%3Fsecret%%3D%s";
-
+public final class GoogleAuthenticatorKey
+{
     /**
      * The secret key in Base32 encoding.
      */
@@ -80,29 +71,11 @@ public final class GoogleAuthenticatorKey {
      * @param code         the verification code at time = 0 (the UNIX epoch).
      * @param scratchCodes the list of scratch codes.
      */
-    /* package */ GoogleAuthenticatorKey(
-            String secretKey, int code,
-            List<Integer> scratchCodes) {
+    /* package */ GoogleAuthenticatorKey(String secretKey, int code, List<Integer> scratchCodes)
+    {
         key = secretKey;
         verificationCode = code;
         this.scratchCodes = new ArrayList<>(scratchCodes);
-    }
-
-    /**
-     * Returns the URL of a Google-provided QR barcode to be loaded into the
-     * Google Authenticator application. The user scans this bar code with the
-     * application on their smart phones or manually enter the secret manually.
-     *
-     * @param user   the user to assign the secret key to.
-     * @param host   the host to assign the secret key to.
-     * @param secret the secret key in Base32 encoding.
-     * @return the URL of a Google-provided QR barcode to be loaded into the
-     * Google Authenticator application.
-     * @deprecated Use GoogleAuthenticatorQRGenerator##getOtpAuthURL instead.
-     */
-    @SuppressWarnings("deprecation, unused")
-    public static String getQRBarcodeURL(String user, String host, String secret) {
-        return String.format(QR_FORMAT, user, host, secret);
     }
 
     /**
@@ -110,7 +83,8 @@ public final class GoogleAuthenticatorKey {
      *
      * @return the list of scratch codes.
      */
-    public List<Integer> getScratchCodes() {
+    public List<Integer> getScratchCodes()
+    {
         return scratchCodes;
     }
 
@@ -119,7 +93,8 @@ public final class GoogleAuthenticatorKey {
      *
      * @return the secret key in Base32 encoding.
      */
-    public String getKey() {
+    public String getKey()
+    {
         return key;
     }
 
@@ -128,7 +103,8 @@ public final class GoogleAuthenticatorKey {
      *
      * @return the verificationCode at time = 0 (the UNIX epoch).
      */
-    public int getVerificationCode() {
+    public int getVerificationCode()
+    {
         return verificationCode;
     }
 }
